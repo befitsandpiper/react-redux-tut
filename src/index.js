8abdae6eae8ef54407a16b75b1ef4ef88488e053
befitsandpiper <befitsandpiper@gmail.com>
2016-09-18 22:34:55 -0700

    lecture 29

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ytSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyBblkHmaK7iy6x8YeOQpmSeD0TRO0_mbLI';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// most parent component should be responsible for fetching the data
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = { videos: [] };
+
+    ytSearch({ key: API_KEY, term: 'hedgehogs' },
+      videos => this.setState({ videos }));
+      // this.setState({ videos : videos})
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
